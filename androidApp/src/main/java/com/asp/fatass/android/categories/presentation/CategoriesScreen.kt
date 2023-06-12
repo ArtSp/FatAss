7cc5c19083fdf7e7a8be3fbfa36c07c98d871b2d
Artjoms Spole <artjoms.spole@gmail.com>
2023-06-12 11:05:13 +0300

    main; android errors

--- androidApp/src/main/java/com/asp/fatass/android/categories/presentation/CategoriesScreen.kt
+++ androidApp/src/main/java/com/asp/fatass/android/categories/presentation/CategoriesScreen.kt
@@ -1,9 +1,14 @@
 package com.asp.fatass.android.categories.presentation
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import com.asp.fatass._core.domain.util.error.AppError
+import com.asp.fatass.android.R
 import com.asp.fatass.categories.presentation.CategoriesEvent
 import com.asp.fatass.categories.presentation.CategoriesState
+import com.asp.fatass.products.presentation.ProductsEvent
 
 @Composable
 fun CategoriesScreen(
@@ -12,4 +17,17 @@ fun CategoriesScreen(
 ) {
     val context = LocalContext.current
 
+    LaunchedEffect(key1 = state.error) {
+        val message = when(state.error) {
+            is AppError.ServiceUnavailable -> context.getString(R.string.error_service_unavailable)
+            is AppError.ClientError -> context.getString(R.string.client_error)
+            is AppError.ServerError -> context.getString(R.string.server_error)
+            is AppError.UnknownError -> context.getString(R.string.unknown_error)
+            else -> null
+        }
+        message?.let {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            onEvent(CategoriesEvent.ClearError)
+        }
+    }
 }
--- androidApp/src/main/java/com/asp/fatass/android/products/presentation/ProductsScreen.kt
+++ androidApp/src/main/java/com/asp/fatass/android/products/presentation/ProductsScreen.kt
@@ -1,7 +1,11 @@
 package com.asp.fatass.android.products.presentation
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import com.asp.fatass._core.domain.util.error.AppError
+import com.asp.fatass.android.R
 import com.asp.fatass.products.presentation.ProductsEvent
 import com.asp.fatass.products.presentation.ProductsState
 
@@ -11,4 +15,18 @@ fun ProductsScreen(
     onEvent: (ProductsEvent) -> Unit
 ) {
     val context = LocalContext.current
+
+    LaunchedEffect(key1 = state.error) {
+        val message = when(state.error) {
+            is AppError.ServiceUnavailable -> context.getString(R.string.error_service_unavailable)
+            is AppError.ClientError -> context.getString(R.string.client_error)
+            is AppError.ServerError -> context.getString(R.string.server_error)
+            is AppError.UnknownError -> context.getString(R.string.unknown_error)
+            else -> null
+        }
+        message?.let {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            onEvent(ProductsEvent.ClearError)
+        }
+    }
 }
